e298490bffeac88fa22e1b2273c8eb6018b2db61
Ickik <p.allgeyer@gmail.com>
2012-06-01 16:13:21 +0200

    Add log

--- Updater/src/main/java/fr/ickik/updater/ExecutionThreadGroup.java
+++ Updater/src/main/java/fr/ickik/updater/ExecutionThreadGroup.java
@@ -26,11 +26,52 @@ final class ExecutionThreadGroup extends ThreadGroup {
 	}
 
 	public void uncaughtException(Thread t, Throwable e) {
-		JOptionPane.showMessageDialog(findActiveFrame(), e.toString(), " - Exception Occurred", JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(findActiveFrame(), e.toString(), "FormulaMath - Exception Occurred", JOptionPane.ERROR_MESSAGE);
 		log.error(e.getMessage());
 		log.error("Cause : {}", e.getCause().getMessage());
 		e.printStackTrace();
+		System.exit(1);
 	}
+	
+//	private void sendMail(String errorTitle) {
+//	try {
+//		Properties prop = System.getProperties();
+//		prop.put("mail.smtp.host", "");
+//		Session session = Session.getDefaultInstance(prop,null);
+//		Message message = new MimeMessage(session);
+//		message.setFrom(new InternetAddress("formulamath@gmail.com"));
+//		InternetAddress[] internetAddresses = new InternetAddress[1];
+//		internetAddresses[0] = new InternetAddress("formulamath@gmail.com");
+//		message.setRecipients(Message.RecipientType.TO,internetAddresses);
+//		message.setSubject("Error on FormulaMath : " + errorTitle);
+//		message.setText("test mail");
+//		message.setHeader("X-Mailer", "Java");
+//		
+//		message.setContent(getLogAttachment());
+//		message.setSentDate(new Date());
+//		session.setDebug(true);
+//		Transport.send(message);
+//	} catch (AddressException e) {
+//		e.printStackTrace();
+//	} catch (MessagingException e) {
+//		e.printStackTrace();
+//	}
+//}
+//
+//private Multipart getLogAttachment() {
+//	Multipart multiPart = new MimeMultipart();
+//	BodyPart bodypart = new MimeBodyPart();
+//	FileDataSource source = new FileDataSource(new File(System.getProperty("user.home") + "/.FormulaMath/log/FormulaMath.log"));
+//	try {
+//		bodypart.setFileName("FormulaMath.log");
+//		bodypart.setDataHandler(new DataHandler(source));
+//		multiPart.addBodyPart(bodypart);
+//	} catch (MessagingException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//	return multiPart;
+//}
 
 	private Frame findActiveFrame() {
 		Frame[] frames = JFrame.getFrames();

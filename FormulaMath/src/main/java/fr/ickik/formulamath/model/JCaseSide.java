f65d2a567b7c1cf25702932772a61d8b3125bb8d
Ickik <p.allgeyer@gmail.com>
2012-07-09 17:07:01 +0200

    comments

--- FormulaMath/src/main/java/fr/ickik/formulamath/model/JCaseSide.java
+++ FormulaMath/src/main/java/fr/ickik/formulamath/model/JCaseSide.java
@@ -8,13 +8,64 @@ package fr.ickik.formulamath.model;
  */
 public enum JCaseSide {
 
+	/**
+	 * Bottom side of the JCase.
+	 */
 	BOTTOM,
-	BOTTOM_LEFT_CORNER,
-	BOTTOM_RIGHT_CORNER,
+	
+	/**
+	 * The Left bottom corner but the direction is from NORTH to EAST (left) the angle is inside.
+	 */
+	BOTTOM_LEFT_CORNER_ACUTE,
+	
+	/**
+	 * The Left bottom corner but the direction is from SOUTH to WEST (left) the angle is outside.
+	 */
+	BOTTOM_LEFT_CORNER_REFLEX,
+	
+	/**
+	 * The Right bottom corner but the direction is from NORTH to WEST (right) the angle is inside.
+	 */
+	BOTTOM_RIGHT_CORNER_ACUTE,
+	
+	/**
+	 * The Right bottom corner but the direction is from SOUTH to EAST (right) the angle is outside.
+	 */
+	BOTTOM_RIGHT_CORNER_REFLEX,
+	
+	/**
+	 * Left side of the JCase.
+	 */
 	LEFT,
+	
+	/**
+	 * Right side of the JCase.
+	 */
 	RIGHT,
+	
+	/**
+	 * Top side of the JCase.
+	 */
 	TOP,
-	TOP_LEFT_CORNER,
-	TOP_RIGHT_CORNER;
+	
+	/**
+	 * The Top Left corner but the direction is from SOUTH to EAST (right) the angle is inside.
+	 */
+	TOP_LEFT_CORNER_ACUTE,
+	
+	/**
+	 * The Top Left corner but the direction is from NORTH to WEST (right) the angle is outside.
+	 */
+	TOP_LEFT_CORNER_RELFEX,
+	
+	/**
+	 * The Top Right corner but the direction is from SOUTH to WEST (left) the angle is inside.
+	 */
+	TOP_RIGHT_CORNER_ACUTE,
+	
+	/**
+	 * The Top Right corner but the direction is from NORTH to EAST (left) the angle is outside.
+	 */
+	TOP_RIGHT_CORNER_REFLEX;
 	
 }

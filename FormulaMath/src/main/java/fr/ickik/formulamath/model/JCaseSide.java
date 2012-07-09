6f55fcec90017d4099a0efa3cb39e83a6d2f5739
Ickik <p.allgeyer@gmail.com>
2012-07-09 11:17:11 +0200

    Add position enum for drawing in a case at a precise position Removing unused object

---
+++ FormulaMath/src/main/java/fr/ickik/formulamath/model/JCaseSide.java
@@ -0,0 +1,20 @@
+package fr.ickik.formulamath.model;
+
+/**
+ * This enum has all positions in a JCase to draw at this place.
+ * @author ickik
+ * @version 0.1.000, 9 July 2012
+ * @since 0.3.7
+ */
+public enum JCaseSide {
+
+	BOTTOM,
+	BOTTOM_LEFT_CORNER,
+	BOTTOM_RIGHT_CORNER,
+	LEFT,
+	RIGHT,
+	TOP,
+	TOP_LEFT_CORNER,
+	TOP_RIGHT_CORNER;
+	
+}

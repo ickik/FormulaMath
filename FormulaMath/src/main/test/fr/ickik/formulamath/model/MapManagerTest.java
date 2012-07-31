6af6db092c0f00febe79aaf71b353f40f4028ad2
Ickik <p.allgeyer@gmail.com>
2012-07-31 15:45:20 +0200

    Add test to mapManager

--- FormulaMath/src/main/test/fr/ickik/formulamath/model/MapManagerTest.java
+++ FormulaMath/src/main/test/fr/ickik/formulamath/model/MapManagerTest.java
@@ -1,42 +1,67 @@
 package fr.ickik.formulamath.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
+import static org.testng.Assert.assertNotNull;
 
 import org.testng.annotations.Test;
 
+import fr.ickik.formulamath.model.map.Field;
 import fr.ickik.formulamath.model.map.MapManager;
 
 public class MapManagerTest {
 
-	public static void main(String[] args) {
-		try {
-		final int size = 100;
-		List<String> l = new ArrayList<String>();
-		for (int i = 0; i < 1000; i++) {
-		long t = System.currentTimeMillis();
-		MapManager manager = new MapManager();
-		manager.init(size);
-		manager.constructRoad();
-		l.add((System.currentTimeMillis() - t) + "ms   number of direction :" + manager.getRoadDirectionInformationList().size());
-		}
-		for (String s : l) {
-			System.out.println(s);
-		}
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
+	private final int size = 50;
 
 	@Test
-	public void MapGeneration() {
-		final int size = 100;
+	public void mapGeneration() {
+		MapManager manager = new MapManager();
+		manager.init(size);
+		assertEquals(manager.getMapSize(), size);
+		assertEquals(manager.getRoadDirectionInformationList().size(), 0);
+		assertEquals(manager.getFinishingLinePositionList().size(), 0);
+		assertEquals(manager.getStartingPositionList().size(), 0);
+		for (int i = 0; i< size ; i++) {
+			for (int j = 0; j< size ; j++) {
+				assertNotNull(manager.getCase(i, j).getField());
+				assertNotEquals(manager.getCase(i, j).getField(), Field.STARTING_LINE);
+				assertNotEquals(manager.getCase(i, j).getField(), Field.ROAD);
+				assertNotEquals(manager.getCase(i, j).getField(), Field.FINISHING_LINE);
+			}
+		}
+	}
+	
+	@Test
+	public void mapGenerationConstruction() {
+		MapManager manager = new MapManager();
+		manager.init(size);
+		assertEquals(manager.getMapSize(), size);
+		manager.constructRoad();
+		assertNotEquals(manager.getRoadDirectionInformationList().size(), 0);
+		assertEquals(manager.getFinishingLinePositionList().size(), 2);
+		assertEquals(manager.getStartingPositionList().size(), MapManager.ROAD_SIZE);
+	}
+	
+	@Test
+	public void mapReinitialisation() {
 		MapManager manager = new MapManager();
 		manager.init(size);
 		manager.constructRoad();
-		String mapString = manager.toString();
-		String[] map = mapString.split("\n");
-		//assertEquals(map.length, size);
+		int roadSize = manager.getRoadDirectionInformationList().size();
+		manager.reinitialization();
+		assertEquals(manager.getRoadDirectionInformationList().size(), roadSize);
+		assertEquals(manager.getFinishingLinePositionList().size(), 2);
+		assertEquals(manager.getStartingPositionList().size(), MapManager.ROAD_SIZE);
+	}
+	
+	@Test
+	public void mapFullReinitialisation() {
+		MapManager manager = new MapManager();
+		manager.init(size);
+		manager.constructRoad();
+		manager.fullReinitialization();
+		assertEquals(manager.getRoadDirectionInformationList().size(), 0);
+		assertEquals(manager.getFinishingLinePositionList().size(), 0);
+		assertEquals(manager.getStartingPositionList().size(), 0);
 	}
 }

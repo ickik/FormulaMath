6b92fbb8487de379545a31e70fd9678ebd43f8f0
Ickik <p.allgeyer@gmail.com>
2012-08-02 09:43:56 +0200

    Change version number

--- Updater/src/main/java/fr/ickik/updater/Updater.java
+++ Updater/src/main/java/fr/ickik/updater/Updater.java
@@ -14,7 +14,7 @@ import fr.ickik.updater.model.UpdateModel;
  */
 public final class Updater {
 
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.1";
 	private static final Logger logger = LoggerFactory.getLogger(Updater.class);
 	
 	/**

10c9176c4daf610c4a6845efa1fcb3e70d3d658c
Ickik <p.allgeyer@gmail.com>
2012-09-03 16:55:31 +0200

    Creation of window listener of save properties after frame closing

--- FormulaMath/src/main/java/fr/ickik/formulamath/view/ConfigurationFrame.java
+++ FormulaMath/src/main/java/fr/ickik/formulamath/view/ConfigurationFrame.java
@@ -7,6 +7,8 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,7 +41,7 @@ import fr.ickik.formulamath.model.player.PlayerType;
  * The user can choose between Human and Computer players;
  * give a name to every player.
  * @author Ickik.
- * @version 0.2.010, 31 August 2012
+ * @version 0.2.011, 3rd September 2012
  */
 public class ConfigurationFrame extends AbstractFormulaMathFrame {
 
@@ -82,6 +84,37 @@ public class ConfigurationFrame extends AbstractFormulaMathFrame {
 		} else {
 			log.debug("Help file not found");
 			panel.add(getButton(), BorderLayout.SOUTH);
+		}
+		if (getFrame().getWindowListeners().length == 0) {
+			getFrame().addWindowListener(new WindowListener() {
+
+				@Override
+				public void windowOpened(WindowEvent arg0) {}
+
+				@Override
+				public void windowIconified(WindowEvent arg0) {}
+
+				@Override
+				public void windowDeiconified(WindowEvent arg0) {}
+
+				@Override
+				public void windowDeactivated(WindowEvent arg0) {}
+
+				@Override
+				public void windowClosing(WindowEvent arg0) {
+					try {
+						controller.saveProperties();
+					} catch (FormulaMathException e) {
+						displayErrorMessage(e.getMessage());
+					}
+				}
+
+				@Override
+				public void windowClosed(WindowEvent arg0) {}
+
+				@Override
+				public void windowActivated(WindowEvent arg0) {}
+			});
 		}
 		getFrame().add(panel);
 	}

3acbdf3f41929ed34411149999de9fda2cbd698f
Ickik <p.allgeyer@gmail.com>
2012-09-04 15:03:35 +0200

    serial number frame and model

---
+++ FormulaMath/src/main/java/fr/ickik/formulamath/model/UserModel.java
@@ -0,0 +1,139 @@
+package fr.ickik.formulamath.model;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * 
+ * @author Ickik
+ * @version 0.1.000, 4th September 2012
+ * @since 0.3.10
+ */
+public final class UserModel {
+
+	public boolean isKeyValide() {
+		String key = "";
+		return isKeyValide(key);
+	}
+	
+	public boolean isKeyValide(String key) {
+		String serialNumberEncoded = getSerialNumber();
+		return key.equalsIgnoreCase(serialNumberEncoded);
+	}
+	
+	public void keyRequest(String emailAddress) {
+		/*try {
+			Properties prop = System.getProperties();
+			prop.put("mail.smtps.host", "smtp.gmail.com");
+			prop.put("mail.smtps.port", "465");
+			prop.put("mail.smtps.auth", "true");
+			Session session = Session.getDefaultInstance(prop,null);
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("formulamath@gmail.com"));
+			InternetAddress[] internetAddresses = new InternetAddress[1];
+			internetAddresses[0] = new InternetAddress("formulamath@gmail.com");
+			message.setRecipients(Message.RecipientType.TO,internetAddresses);
+			message.setSubject("Key request");
+			message.setText(getReversedMacAddress());
+			message.setHeader("X-Mailer", "Java");
+			message.setSentDate(new Date());
+			session.setDebug(true);
+			Transport.send(message);
+		} catch (AddressException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}*/
+	}
+
+	private String getReversedMacAddress() {
+		try {
+			byte[] array = NetworkInterface.getByIndex(0).getHardwareAddress();
+			StringBuilder p = new StringBuilder();
+			for (byte b : array){
+				p.append(String.format("%02X",b));
+			}
+			return p.reverse().toString();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private String getSerialNumber() {
+		try {
+			String mac = getReversedMacAddress();
+			//225
+			String encodedSerialNumber = calculateSecurityHash(mac,"SHA-512") + calculateSecurityHash(mac,"MD5") + calculateSecurityHash(mac,"SHA-256");
+
+			System.out.println(encodedSerialNumber);
+			
+			String hashedSerialNumber = "" + encodedSerialNumber.charAt(66)
+					+ encodedSerialNumber.charAt(51)
+					+ encodedSerialNumber.charAt(79)
+					+ encodedSerialNumber.charAt(186)
+					+ encodedSerialNumber.charAt(202)
+					+ encodedSerialNumber.charAt(3)
+					+ encodedSerialNumber.charAt(191)
+					+ encodedSerialNumber.charAt(83)
+					+ encodedSerialNumber.charAt(36)
+					+ encodedSerialNumber.charAt(218)
+					+ encodedSerialNumber.charAt(47)
+					+ encodedSerialNumber.charAt(123)
+					+ encodedSerialNumber.charAt(177)
+					+ encodedSerialNumber.charAt(12)
+					+ encodedSerialNumber.charAt(130)
+					+ encodedSerialNumber.charAt(94)
+					+ encodedSerialNumber.charAt(53)
+					+ encodedSerialNumber.charAt(102)
+					+ encodedSerialNumber.charAt(15)
+					+ encodedSerialNumber.charAt(169);
+			String reEncodedSerialNumber = calculateSecurityHash(hashedSerialNumber,"SHA-512");
+			String reHashedSerialNumber = reEncodedSerialNumber.charAt(54)
+					+ reEncodedSerialNumber.charAt(78)
+					+ reEncodedSerialNumber.charAt(98)
+					+ reEncodedSerialNumber.charAt(101)
+					+ reEncodedSerialNumber.charAt(4)
+					+ "-"
+					+ reEncodedSerialNumber.charAt(22)
+					+ reEncodedSerialNumber.charAt(13)
+					+ reEncodedSerialNumber.charAt(83)
+					+ reEncodedSerialNumber.charAt(124)
+					+ reEncodedSerialNumber.charAt(61)
+					+ "-"
+					+ reEncodedSerialNumber.charAt(52)
+					+ reEncodedSerialNumber.charAt(47)
+					+ reEncodedSerialNumber.charAt(117)
+					+ reEncodedSerialNumber.charAt(121)
+					+ reEncodedSerialNumber.charAt(32)
+					+ "-"
+					+ reEncodedSerialNumber.charAt(26)
+					+ reEncodedSerialNumber.charAt(38)
+					+ reEncodedSerialNumber.charAt(86)
+					+ reEncodedSerialNumber.charAt(116)
+					+ reEncodedSerialNumber.charAt(59);
+			return reHashedSerialNumber.toUpperCase();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private String calculateSecurityHash(String stringInput, String algorithmName) throws NoSuchAlgorithmException {
+		String hexMessageEncode = "";
+		byte[] buffer = stringInput.getBytes();
+		MessageDigest messageDigest = MessageDigest.getInstance(algorithmName);
+		messageDigest.update(buffer);
+		byte[] messageDigestBytes = messageDigest.digest();
+		for (int index=0; index < messageDigestBytes.length ; index ++) {
+			int countEncode = messageDigestBytes[index] & 0xff;
+			if (Integer.toHexString(countEncode).length() == 1) hexMessageEncode = hexMessageEncode + "0";
+			hexMessageEncode = hexMessageEncode + Integer.toHexString(countEncode);
+		}
+		return hexMessageEncode;
+	}
+}
---
+++ FormulaMath/src/main/java/fr/ickik/formulamath/view/SerialNumberFrame.java
@@ -0,0 +1,177 @@
+package fr.ickik.formulamath.view;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.regex.Pattern;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import fr.ickik.formulamath.FormulaMathException;
+import fr.ickik.formulamath.controler.FormulaMathController;
+import fr.ickik.formulamath.model.JTextFieldLimit;
+import fr.ickik.formulamath.model.UserModel;
+
+/**
+ * 
+ * @author Ickik
+ * @version 0.1.000, 4th September 2012
+ * @since 0.3.10
+ */
+public final class SerialNumberFrame extends AbstractFormulaMathFrame {
+
+	private final FormulaMathController controller;
+	private final int textSlotNumber = 5;
+	private final JTextField emailTextField = new JTextField(20);
+	private final JTextField[] textFieldArray = new JTextField[textSlotNumber];
+	private final UserModel model;
+	
+	public SerialNumberFrame(UserModel model, FormulaMathController controller) {
+		this.controller = controller;
+		this.model = model;
+		getFrame().addWindowListener(createWindowListener());
+		
+	}
+	
+	public void display() {
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(getPanel(), BorderLayout.CENTER);
+		panel.add(getButtons(), BorderLayout.SOUTH);
+		getFrame().add(panel);
+		displayFrame();
+	}
+	
+	private WindowListener createWindowListener() {
+		return new WindowListener() {
+			
+			@Override
+			public void windowOpened(WindowEvent arg0) {}
+			@Override
+			public void windowIconified(WindowEvent arg0) {}
+			@Override
+			public void windowDeiconified(WindowEvent arg0) {}
+			@Override
+			public void windowDeactivated(WindowEvent arg0) {}
+			@Override
+			public void windowClosing(WindowEvent arg0) {}
+			
+			@Override
+			public void windowClosed(WindowEvent arg0) {
+				try {
+					controller.saveProperties();
+				} catch (FormulaMathException e) {
+					displayErrorMessage(e.getMessage());
+				}
+			}
+			
+			@Override
+			public void windowActivated(WindowEvent arg0) {}
+		};
+	}
+	
+	private JPanel getButtons() {
+		JPanel panel = new JPanel(new GridLayout());
+		JButton validate = new JButton("Validate");
+		validate.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				StringBuilder key = new StringBuilder();
+				for (int i = 0; i < textSlotNumber; i++) {
+					key.append(textFieldArray[i].getText());
+					if (i < textSlotNumber - 1) {
+						key.append('-');
+					}
+				}
+				if (model.isKeyValide(key.toString())) {
+					controller.openConfigurationFrame();
+				} else {
+					displayErrorMessage("Key invalide");
+				}
+				//controller.enterKey(key.toString());
+			}
+		});
+		
+		JButton reset = new JButton("Reset");
+		reset.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				for (JTextField field : textFieldArray) {
+					field.setText("");
+				}
+			}
+		});
+		
+		JButton cancel = new JButton("Cancel");
+		cancel.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		panel.add(validate);
+		panel.add(reset);
+		panel.add(cancel);
+		return panel;
+	}
+	
+	private JPanel getPanel() {
+		JPanel panel = new JPanel();
+		BoxLayout layout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+		panel.setLayout(layout);
+		
+		JPanel emailPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		emailPanel.add(new JLabel("Email :"));
+		this.emailTextField.setText("");
+		emailPanel.add(this.emailTextField);
+		emailPanel.add(getRequestKeyButton());
+		panel.add(emailPanel);
+		JPanel entryPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		entryPanel.add(new JLabel("Serial number : "));
+		final int textSlotSize = 6;
+		for (int i = 0; i < textSlotNumber; i++) {
+			JTextField textField = new JTextField(textSlotSize);
+			textField.setDocument(new JTextFieldLimit(textSlotSize));
+			textFieldArray[i] = textField;
+			entryPanel.add(textField);
+			if (i < textSlotNumber - 1) {
+				entryPanel.add(new JLabel("-"));
+			}
+		}
+		panel.add(entryPanel);
+		return panel;
+	}
+	
+	private JButton getRequestKeyButton() {
+		JButton button = new JButton("Request Key");
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String email = emailTextField.getText();
+				if (isEmailValide(email)) {
+					model.keyRequest(email);
+				} else {
+					displayErrorMessage("Email not valide");
+				}
+			}
+		});
+		return button;
+	}
+	
+	private boolean isEmailValide(String emailAddress) {
+		Pattern pattern = Pattern.compile("^[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\\.)+[A-Z]{2,4}$");
+		return pattern.matcher(emailAddress).matches();
+	}
+	
+}

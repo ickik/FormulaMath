fd995fc99b84e28f7bd0713ee95ba7f2aedb5b39
Ickik <p.allgeyer@gmail.com>
2012-09-06 16:47:02 +0200

    Correction

--- FormulaMath/src/main/java/fr/ickik/formulamath/model/ai/AIEasyLevel.java
+++ FormulaMath/src/main/java/fr/ickik/formulamath/model/ai/AIEasyLevel.java
@@ -1,5 +1,6 @@
 package fr.ickik.formulamath.model.ai;
 
+import java.awt.geom.Line2D;
 import java.util.List;
 import java.util.Map;
 
@@ -12,8 +13,11 @@ import fr.ickik.formulamath.entity.Player;
 import fr.ickik.formulamath.entity.Position;
 import fr.ickik.formulamath.entity.Vector;
 import fr.ickik.formulamath.model.CaseModel;
+import fr.ickik.formulamath.model.map.Field;
 import fr.ickik.formulamath.model.map.MapManager;
 import fr.ickik.formulamath.model.map.Orientation;
+import fr.ickik.formulamath.view.JCase;
+import fr.ickik.formulamath.view.MainFrame;
 
 /**
  * Computer Easy Level. This class choose a simple vector to move. <br>What is a simple Vector?
@@ -98,13 +102,54 @@ public final class AIEasyLevel implements AILevel {
 				isLastPlay = true;
 			}
 		}
+		
+		int marge = MainFrame.MAP_MARGIN / 2;
+		int xTrayPanel = player.getPosition().getX() + marge;
+		int yTrayPanel = player.getPosition().getY() + marge;
+		JCase c = mapManager.getCarteComponent().get(yTrayPanel).get(xTrayPanel);
+		int y = getCoordinateLimit(yTrayPanel - vector.getY(), mapManager.getMapSize());
+		int x = getCoordinateLimit(xTrayPanel + vector.getX(), mapManager.getMapSize());
+		log.trace("Player final  position on map : ( {}, {} )", x, y);
+
+		final JCase c2 = mapManager.getCarteComponent().get(y).get(x);
+		Line2D line = new Line2D.Double(c.getX() + (c.getWidth() / 2), c.getY() + (c.getHeight() / 2), c2.getX() + (c.getWidth() / 2), c2.getY() + (c.getHeight() / 2));
+		log.trace("Vector's line on map from ( {}, {} ) to ( {} , {} )", new Object[]{c.getX(), c.getY(), c2.getX(), c2.getY()});
+		if (isGrassIntersection(line)) {
+			log.trace("{} intersects grass", vector.toString());
+			vector = new Vector(0, 0);
+		}
+		if (isEndLineIntersection(line)) {
+			log.trace("{} intersects finish line", vector.toString());
+			isLastPlay = true;
+			return vector;
+		}
+		
 		if (player != null && player.getPosition() != null && vector != null) {
-			CaseModel c = mapManager.getCase(player.getPosition().getY() + vector.getY(), player.getPosition().getX()  + vector.getX());
-			if (c != null && c.isOccuped()) {
+			CaseModel model = mapManager.getCase(player.getPosition().getY() + vector.getY(), player.getPosition().getX()  + vector.getX());
+			if (model != null && model.isOccuped()) {
 				vector = new Vector(0, 0);
 			}
 		}
 		return vector;
+	}
+	
+	private int getCoordinateLimit(int coordinate, int mapSize) {
+		if (coordinate < MainFrame.MAP_MARGIN / 2) {
+			return MainFrame.MAP_MARGIN / 2;
+		} else if (coordinate >= mapSize + (MainFrame.MAP_MARGIN / 2)) {
+			return mapSize + (MainFrame.MAP_MARGIN / 2) - 1;
+		}
+		return coordinate;
+	}
+	
+	private boolean isEndLineIntersection(Line2D shape) {
+		log.debug("isEndLineIntersection");
+		return mapManager.checkIntersection(shape, Field.FINISHING_LINE);
+	}
+	
+	private boolean isGrassIntersection(Line2D shape) {
+		log.debug("isGrassIntersection");
+		return mapManager.checkIntersection(shape, Field.GRASS);
 	}
 
 	@Override
